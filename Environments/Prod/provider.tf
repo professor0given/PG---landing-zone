f6cf11402b121cd52bf35318b54d92fef3f389b4
Rohit <Rohit@smartworlddevelopers.com>
2026-08-23 11:48:53 +0530

    added backend for tfstate

--- Environments/Prod/provider.tf
+++ Environments/Prod/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.1.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "stg-pg-lz-rg"
+    storage_account_name = "stgpglzstate001"
+    container_name       = "tfstate"
+    key                  = "prod.tfstate"
+  }
 }
 
 provider "azurerm" {
